524cc2f90953724972b7afca70adb8dd642e98a3
Joe <joseph.lee3454@gmail.com>
2020-12-15 20:40:10 -0800

    added some more stuff

--- show/models.py
+++ show/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class ShowManager(models.Manager):
+       # No fields should be a blank 
+        # title at east two characters
+        # network " 3 chars
+        # desc 10
+    def validateShow(self, postData):
+        errors = {}
+
+        if len(postData["release"]) <=10:
+            errors["release"] = "Release date must not be blank"
+        if len(postData["title"]) <2:
+            errors["title"] = "Title msut be more than 2 char"
+        if len(postData["network"]) <3:
+            errors["network"] = "Title msut be more than 2 char"
+        if len(postData["desc"]) <10:
+            errors["desc"] = "Title msut be more than 2 char"
+        return errors
+
+     
+
+
+
+
+
 class NewShow(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
@@ -8,6 +32,8 @@ class NewShow(models.Model):
     release = models.DateField()
     desc = models.TextField()
 
+    objects = ShowManager()
+
     def __str__(self):
        return self.title + " " + self.network + " "  + self.desc + f'{self.id}'
-  
+   
--- show/views.py
+++ show/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -8,15 +9,22 @@ def index(request):
 
 def shows(request): # this function is for my table of items in the db 
     context = {
-        "all_shows": NewShow.objects.all()#says i want all items o the model NewShow
+        "all_shows": NewShow.objects.all()#says i want all items in the model NewShow
     }
 
     return render(request, 'shows.html', context)
 
 def add_show(request):#this method creates a new model element 
+
     if request.method == "POST":
-        show = NewShow.objects.create(title = request.POST['title'], network = request.POST['network'], release = request.POST['release'], desc = request.POST['desc'])
-        return redirect(f"show_info/{show.id}")#redirect to show info and passes the id throughht the f string 
+        errors = NewShow.objects.validateShow(request.POST)
+        if errors:
+            for key, value in errors.items():# loops through the dictionary off errors
+                messages.error(request, value)# gives a message for every error in dict 
+            return redirect("/")# redirect back to home page becuase we want to finish 
+        else:
+            show = NewShow.objects.create(title = request.POST['title'], network = request.POST['network'], release = request.POST['release'], desc = request.POST['desc'])
+            return redirect(f"show_info/{show.id}")#redirect to show info and passes the id throughht the f string 
 
 
 def show_info(request,show_id):#this method is for my hyper link to show each specific show by its id 
@@ -29,10 +37,10 @@ def show_info(request,show_id):#this method is for my hyper link to show each sp
 
 def delete_show(request,show_id):# this method is for my deletion of a element 
     show = NewShow.objects.get(id=show_id)
-    show.delete()
+    show.delete()#delete method this builtin method will delete the objs you put on a varible 
     return redirect('/shows')
 
-def edit_show(request, show_id):
+def edit_show(request, show_id):## this functions sole purpose is to bring us to a page with our old value already in the fields 
     context = {
     'show': NewShow.objects.get(id=show_id),
     'all_shows': NewShow.objects.all()
@@ -40,14 +48,21 @@ def edit_show(request, show_id):
     }
     return render(request, 'edit.html', context)
 
-def show_update(request, show_id):
-    update_show = NewShow.objects.get(id=show_id)
-    update_show.title = request.POST['title']
-    update_show.network = request.POST['network']
-    update_show.release = request.POST['release']
-    update_show.desc = request.POST['desc']
-    update_show.save()
-    return redirect(f"show_info/{show.id}")
+def show_update(request, show_id):# this function actually updates by sending a new post request that overwrites the old db info
+        if request.method == "POST":
+            errors = NewShow.objects.validateShow(request.POST)
+            if errors:
+                for key, value in errors.items():
+                    messages.error(request, value)
+                return redirect("/")
+            else:
+                update_show = NewShow.objects.get(id=show_id)
+                update_show.title = request.POST['title']
+                update_show.network = request.POST['network']
+                update_show.release = request.POST['release']
+                update_show.desc = request.POST['desc']
+                update_show.save()
+                return redirect(f"show_info/{show.id}")
 
 
 
